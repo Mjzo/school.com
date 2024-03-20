2b463b23d012d89f4b7a333927c6c74b81f85c32
Mjzo <josephmusembimukula@gmail.com>
2024-03-20 17:17:51 +0300

    tracking system

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+  <strong>parent-student progress tracking system {{ date('Y') }} </strong>
+
+  
+</footer>
+
+<aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
